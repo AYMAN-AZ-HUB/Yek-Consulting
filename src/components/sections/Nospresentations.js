e6a264d6c29df8a79c717fb5ed3d1f9768ca038e
AYMAN-AZ-HUB <a36282303@gmail.com>
2024-06-03 16:34:51 +0100

    NOS PRESTATIONS EN DROIT DE L'ÉNERGIE fixed in responsive

--- src/components/sections/Nospresentations.js
+++ src/components/sections/Nospresentations.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import { FaSearch } from "react-icons/fa";
-import { PiLightbulbFilament } from "react-icons/pi";
-import { FaBalanceScale } from "react-icons/fa";
-import { FaPenFancy } from "react-icons/fa";
-import { FaRegHandshake } from "react-icons/fa";
- 
-const IconRow = () => {
+import { FaSearch, FaBalanceScale, FaPenFancy, FaRegHandshake } from 'react-icons/fa';
+import { PiLightbulbFilament } from 'react-icons/pi';
+
+const ResponsiveComponent = () => {
   return (
     <div className="mt-12 text-center">
       <h1 className="text-4xl uppercase font-extrabold mb-12 text-black-800 mt-8">Nos prestations en droit de l'énergie</h1>
-      <div className="flex flex-wrap justify-center items-center space-x-16">
+      <div className="flex flex-wrap justify-center items-center gap-8 lg:gap-16">
         <div className="text-center transform transition-transform duration-300 hover:scale-110">
           <FaSearch className="w-16 h-16 cursor-pointer mx-auto text-[#c8b291]" />
           <p className="mt-4 text-base font-semibold text-gray-700">Droit de l'énergie</p>
         </div>
         <div className="text-center transform transition-transform duration-300 hover:scale-110">
-          <PiLightbulbFilament className="w-16 cursor-pointer h-16 mx-auto text-[#c8b291]" />
+          <PiLightbulbFilament className="w-16 h-16 cursor-pointer mx-auto text-[#c8b291]" />
           <p className="mt-4 text-base font-semibold text-gray-700">Conseil</p>
         </div>
         <div className="text-center transform transition-transform duration-300 hover:scale-110">
@@ -33,6 +29,6 @@ const IconRow = () => {
       </div>
     </div>
   );
-}
- 
-export default IconRow;
+};
+
+export default ResponsiveComponent;
