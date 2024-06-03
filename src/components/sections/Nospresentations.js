bb220c8270e484d84c20b501d2748178584d84c9
AYMAN-AZ-HUB <a36282303@gmail.com>
2024-06-03 16:45:18 +0100

    services updated

--- src/components/sections/Nospresentations.js
+++ src/components/sections/Nospresentations.js
@@ -8,23 +8,19 @@ const ResponsiveComponent = () => {
       <div className="flex flex-wrap justify-center items-center gap-8 lg:gap-16">
         <div className="text-center transform transition-transform duration-300 hover:scale-110">
           <FaSearch className="w-16 h-16 cursor-pointer mx-auto text-[#c8b291]" />
-          <p className="mt-4 text-base font-semibold text-gray-700">Droit de l'énergie</p>
+          <p className="mt-4 text-base font-semibold text-gray-700">Consultation téléphonique</p>
         </div>
         <div className="text-center transform transition-transform duration-300 hover:scale-110">
           <PiLightbulbFilament className="w-16 h-16 cursor-pointer mx-auto text-[#c8b291]" />
-          <p className="mt-4 text-base font-semibold text-gray-700">Conseil</p>
+          <p className="mt-4 text-base font-semibold text-gray-700">Consultation par email </p>
         </div>
         <div className="text-center transform transition-transform duration-300 hover:scale-110">
           <FaBalanceScale className="w-16 h-16 cursor-pointer mx-auto text-[#c8b291]" />
-          <p className="mt-4 text-base font-semibold text-gray-700">Droit administratif</p>
+          <p className="mt-4 text-base font-semibold text-gray-700">Rendez-vous avec un avocat </p>
         </div>
         <div className="text-center transform transition-transform duration-300 hover:scale-110">
           <FaPenFancy className="w-16 h-16 mx-auto cursor-pointer text-[#c8b291]" />
-          <p className="mt-4 text-base font-semibold text-gray-700">Droit des affaires</p>
-        </div>
-        <div className="text-center transform transition-transform duration-300 hover:scale-110">
-          <FaRegHandshake className="w-16 h-16 mx-auto cursor-pointer text-[#c8b291]" />
-          <p className="mt-4 text-base font-semibold text-gray-700">Droit civil</p>
+          <p className="mt-4 text-base font-semibold text-gray-700">Assistance </p>
         </div>
       </div>
     </div>
--- src/components/sections/services/PricingSection.js
+++ src/components/sections/services/PricingSection.js
@@ -10,57 +10,58 @@ export default function PricingSection() {
         <section className="py-12 md:py-16">
             <div className="container mx-auto px-4 md:px-6">
                 <h2 className="text-2xl text-center md:text-3xl font-bold mb-8">Services</h2>
-                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-6">
+ 
                     <PricingCard
-                        title="Autorisations"
+                        title="Conseil "
                         features={[
-                            "Obtention  des autorisations administratives nécessaires à l’équipement énergétique",
-                            "Autorisation d’exploiter, droit des  ICPE",
-                            "Permis de construire",
-                            "Contentieux des autorisations"
-                        ]}
-                        icon={FaPenFancy}
-                    />
-                    <PricingCard
-                        title="Appels d'offres"
-                        features={[
-                            "Assistance à maîtrise d’ouvrage pour la passation des appels d’offres dans le secteur de l’énergie",
-                            "Accompagnement des industriels dans la réponse aux appels d’offre"
-                        ]}
-                        icon={FaBoltLightning}
-                    />
-                    <PricingCard
-                        title="Domaine public"
-                        features={[
-                            "Conclusion de baux requis pour l’équipement énergétique",
-                            "Convention d’occupation du domaine public"
+                            "Rédaction et négociation de contrats ",
+
+                            "Etude de faisabilité et assistance des projets" ,
+                            
+                            "Conseil sur les options de financement ",
+                            
+                            "Conseil sur la conformité aux traités internationaux" 
                         ]}
                         icon={FaMapLocationDot}
                     />
                     <PricingCard
-                        title="Projets citoyens"
+                        title="Contentieux et arbitrage"
                         features={[
-                            "Projets énergétiques portés ou soutenus par des associations",
-                            "Gouvernance des projets",
-                            "Subventions"
+                            "Résolution des litiges contractuels ",
+
+                            "Arbitrage national et international" ,
+                            
+                            "Contestations de décisions réglementaires" ,
+                            
+                            "Litiges relatifs à la conformité environnementale" 
                         ]}
                         icon={FaUsers}
                     />
                     <PricingCard
                         title="Régulation"
                         features={[
-                            "Accès au réseau public de transport ou de distribution",
-                            "Mécanisme de soutien",
-                            "Tarification",
-                            "Autorités de régulation sectorielles (CSA, CRE, ARCEP, ARAF…)"
+                            "Autorisation administrative en matière d’équipement énergétique" ,
+
+                            "Autorisation d’exploiter ",
+                            
+                            "Permis de construire" ,
+                            
+                            "Déclaration IC, ICPE" 
                         ]}
                         icon={FaRegHandshake}
                     />
                     <PricingCard
                         title="Audits"
                         features={[
-                            "Audits réglementaires",
-                            "Contrôle de conformité"
+                            "Contrôle des risques" ,
+
+                            "Evaluation de conformité" ,
+                            
+                            "Audit des contrats" ,
+                            
+                            "Audit environnemental et de sécurité",
+                            
                         ]}
                         icon={FaSearch}
                     />
